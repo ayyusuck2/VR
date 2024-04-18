a7dd34884efc711271ae3f5bc022bfdb071867d4
ayyusuck2 <160296004+ayyusuck2@users.noreply.github.com>
2024-04-18 10:50:53 +0800

    metro

---
+++ emgee/data - 複製.js
@@ -0,0 +1,335 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "好吃的",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.1257660653577677,
+        "pitch": 0.041735785550962134,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.078096567642156,
+          "pitch": 0.1451236576836763,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.014197359491607386,
+          "pitch": 0.1330430596118184,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3702595265654143,
+          "pitch": -0.41292566989091384,
+          "title": "Alpha Dogs",
+          "text": "美式裝潢的熱狗堡店"
+        },
+        {
+          "yaw": -2.6854323689411554,
+          "pitch": -0.5282237605404987,
+          "title": "Tella Tella Cafe",
+          "text": "在二樓，日式裝潢的咖啡廳，每天有不同的限量的甜點"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "登波咖啡",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 3.103419810031056,
+        "pitch": 0.32355632867104944,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4987896402091909,
+          "pitch": 0.2621568994764747,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7770805211500385,
+          "pitch": 0.31053230545191823,
+          "title": "登波咖啡",
+          "text": "店內裝飾很復古，位子不多，肉桂捲很好吃"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "服飾店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.037332392940481185,
+          "pitch": 0.35571535503405016,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9283865252708274,
+          "pitch": 0.10808188930815987,
+          "title": "Woo Planet",
+          "text": "可愛的日系衣服"
+        },
+        {
+          "yaw": 2.743280339057989,
+          "pitch": 0.2443340107942138,
+          "title": "Corban",
+          "text": "韓系服飾"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "乾杯",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 3.102645944626609,
+        "pitch": -0.22756497033589795,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.134844555148881,
+          "pitch": 0.3223982985918781,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.355717597335036,
+          "pitch": 0.45714539429210177,
+          "title": "乾杯",
+          "text": "日式居酒屋"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "飲料店們",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -2.0724682220256145,
+        "pitch": -0.024462318803093908,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2016578608807826,
+          "pitch": 0.2705776579753678,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.511978046361701,
+          "pitch": -0.008047764965910531,
+          "title": "鮮芋仙",
+          "text": ""
+        },
+        {
+          "yaw": 1.4913795794486298,
+          "pitch": 0.1722207699738334,
+          "title": "五桐號",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.002142644707174,
+          "pitch": 0.09265772974825381,
+          "title": "龜記",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "日式丸子",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0994307244181325,
+          "pitch": 0.23843664703387368,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.0455738717881147,
+          "pitch": 0.26332086247154507,
+          "title": "赤丸",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ metro/data - 複製.js
@@ -0,0 +1,59 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-metro",
+      "name": "metro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.73226972631801,
+        "pitch": 0.1790831532999313,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.4767542926398,
+          "pitch": -0.00462465978828952,
+          "title": "中山站一號出口",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.062681592594556,
+          "pitch": 0.027253592687893402,
+          "title": "中山站四號出口",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
