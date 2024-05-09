ea2c806ecea613e0ef0fb53eb08467b532f4f656
ayyusuck2 <160296004+ayyusuck2@users.noreply.github.com>
2024-05-09 11:06:51 +0800

    eat

---
+++ eat/data.js
@@ -0,0 +1,358 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-abcd--a-better-coffee--doughnut-",
+      "name": "ABCD . A Better Coffee & Doughnut ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03370740509669368,
+          "pitch": 0.3021235816836523,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7537948456235171,
+          "pitch": 0.330036969734536,
+          "title": "ABCD . A Better Coffee &amp; Doughnut",
+          "text": "<p>這間是日式甜甜圈專賣店，甜甜圈均一價大概90~110元，個人最推薦檸檬口味的，他們家甜甜圈的內餡都是真材實料的<br></p>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "登波咖啡",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5126681100025916,
+          "pitch": 0.23099861834585234,
+          "rotation": 0,
+          "target": "2-tella-tella-cafe"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3257282829151142,
+          "pitch": 0.2625747514488186,
+          "title": "登波咖啡",
+          "text": "神秘的間咖啡廳，店內位子很少，但是裝潢非常復古很有氛圍感，非常推薦他們家的肉桂捲，現做的超好吃！"
+        }
+      ]
+    },
+    {
+      "id": "2-tella-tella-cafe",
+      "name": "Tella Tella Cafe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.093038925520421,
+          "pitch": 0.23754598470004318,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.00814403352413784,
+          "pitch": 0.19543183296990563,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.7396424464179336,
+          "pitch": -0.5608484242069434,
+          "title": "Tella Tella Cafe",
+          "text": "是一間日式的可愛咖啡廳，裝潢很精緻，甚至有韓團的形象照來這裡取景"
+        },
+        {
+          "yaw": -0.43061888280225524,
+          "pitch": -0.33790978878374744,
+          "title": "Alpha Dogs",
+          "text": "是一間美式的熱狗堡店，店內裝潢很適合拍照！"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "鮮芋仙",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.125120450436615,
+          "pitch": 0.2468124341961726,
+          "rotation": 0,
+          "target": "0-abcd--a-better-coffee--doughnut-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.5231528616509316,
+          "pitch": -0.038921033300677976,
+          "title": "鮮芋仙",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.5462010543664917,
+          "pitch": 0.11651960854972288,
+          "title": "五桐號",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-waku-waku-cafe",
+      "name": "Waku Waku Cafe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.1231075776283648,
+          "pitch": 0.3719507095574741,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "赤丸糰子",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.8480951113471331,
+          "pitch": 0.40879666906531753,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "川渝小吃坊",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2816,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7202871458581788,
+          "pitch": 0.3949779178034909,
+          "rotation": 0,
+          "target": "0-abcd--a-better-coffee--doughnut-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1463124226901957,
+          "pitch": 0.08449663008756048,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
