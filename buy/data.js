d3ee60be0e1cef44a5e37014d543dd8653d16928
ayyusuck2 <160296004+ayyusuck2@users.noreply.github.com>
2024-05-30 11:51:24 +0800

    buy

---
+++ buy/data.js
@@ -0,0 +1,101 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-woo",
+      "name": "woo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.73791665256131,
+          "pitch": 0.12151531263074133,
+          "title": "Corban",
+          "text": "韓系的衣服店"
+        },
+        {
+          "yaw": 2.899706014325207,
+          "pitch": 0.03903600343869584,
+          "title": "Wooplanet",
+          "text": "日系的服飾店"
+        }
+      ]
+    },
+    {
+      "id": "1-toy",
+      "name": "toy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.613158203279241,
+          "pitch": 0.26324621504667434,
+          "title": "扭蛋獵人",
+          "text": "有很多台扭蛋機，及盲何等"
+        }
+      ]
+    }
+  ],
+  "name": "buy",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
