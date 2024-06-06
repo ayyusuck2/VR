44525766009ba88a966debd6a4932a7ca37d5c8b
ayyusuck2 <160296004+ayyusuck2@users.noreply.github.com>
2024-06-06 11:00:08 +0800

    Update data.js

--- metro/data.js
+++ metro/data.js
@@ -53,7 +53,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
